cf919f3a1e87d412b58f614efeda9b1673c362c1
Mahid Sheikh <mahid@standingpad.org>
2025-12-01 15:08:50 -0600

    Check if Typst file exists (and if not, create it)

--- edit-typst-note.sh
+++ edit-typst-note.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# If PDF file already exists, we don't
+# need to wait as long to make sure it
+# exists. We still wait a little bit though
+# for reflex purposes
+if [ ! -f "$1" ]; then
+	touch "$1"
+fi
+
 tmux new-session "nvim $1" \; \
 	new-window "typst watch $1" \; \
-	new-window "sleep 5 && zathura ${1%.typ}.pdf" \; \
+	new-window "sleep 2 && zathura ${1%.typ}.pdf" \; \
 	select-window -t:-2
